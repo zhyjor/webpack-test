241f4d157e31ac31d88b2db7a0086d41c206a224
zhyjor <zhyjor@163.com>
2018-02-05 00:50:50 +0800

    fixed:devServer

--- 1-3/webpack.config.js
+++ 1-3/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         //输出到dist目录下
         //path.resolve是将一个字符解析到一个绝对路径中
         path: path.resolve(__dirname, './dist')
+        //https://segmentfault.com/a/1190000006670084
     },
     //loader加载时顺序是从右到左的的，且基本都支持使用查询方式添加参数
     module: {
@@ -26,18 +27,18 @@ module.exports = {
                 // 因此，我们这样配置后，遇到后缀为.css的文件，webpack先用css-loader加载器去解析这个文件，遇到“@import”等语句就将相应样式文件引入（所以如果没有css-loader，就没法解析这类语句），最后计算完的css，将会使用style-loader生成一个内容为最终解析完的css代码的style标签，放到head标签里。
 
                 // 需要注意的是，loader是有顺序的，webpack肯定是先将所有css模块依赖解析完得到计算结果再创建style标签。因此应该把style-loader放在css-loader的前面（webpack loader的执行顺序是从右到左）。
-                // use: ['css-loader'],
-                use: ExtractTextPlugin.extract({
-                    // 转换.css文件需要的Loader
-                    use: ['css-loader?minimize'],
-                })
+                use: ['style-loader', 'css-loader?minimize'],
+                // use: ExtractTextPlugin.extract({
+                //     // 转换.css文件需要的Loader
+                //     use: ['css-loader?minimize'],
+                // })
             }
         ]
     },
-    plugins: [
-        new ExtractTextPlugin({
-            //从.js文件中提取出来的.css文件的名称
-            filename: `[name]_[contenthash:8].css`,
-        })
-    ]
+    // plugins: [
+    //     new ExtractTextPlugin({
+    //         //从.js文件中提取出来的.css文件的名称
+    //         filename: `[name]_[contenthash:8].css`,
+    //     })
+    // ]
 }
